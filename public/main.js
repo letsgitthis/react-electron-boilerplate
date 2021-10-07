4f824bf6d5a94f0f53b543c7655219c4ecd1a158
letsgitthis <jbentley1011@gmail.com>
2021-10-06 21:11:20 -0500

    bump

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow } = require('electron');
+const {app, BrowserWindow } = require('electron')
 
 require('@electron/remote/main').initialize()
 
@@ -8,6 +8,7 @@ function createWindow() {
         width: 800,
         height: 600,
         webPreferences: {
+            nodeIntegration: true,
             enableRemoteModule: true
         }
     })
@@ -31,5 +32,5 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
     // On OS X it is common to re-create a window in the app when the 
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
